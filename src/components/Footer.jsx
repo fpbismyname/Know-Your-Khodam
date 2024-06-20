b458b1ff77acdcd9bb12214eea46b57bf1f871e5
fpbismyname <fajarpebefajar12@gmail.com>
2024-06-20 21:14:30 +0700

    Update 1.0.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,25 @@ const footer = () => {
   return (
     <>
       <footer className="flex justify-center text-white">
-        <div className="flex px-5 py-2 rounded-full">
+        <div className="flex px-5 pt-10 rounded-full">
           <h1>
             Created by{" "}
             <a href="https://www.instagram.com/fpb_vfx/">
               <strong className="text-pink-400 underline">
                 FPB.JPEG <i className="bi bi-instagram"></i>
               </strong>
-            </a>{" "}
-            |{" "}
-          </h1>
-          <h1>
-            Sawer Gua{" "}
-            <a href="https://saweria.co/FajarSatBoy">
-              <strong className="underline text-green-300">FajarSatBoy <i class="bi bi-coin"></i></strong>
             </a>
           </h1>
         </div>
       </footer>
+      <div className="flex justify-center">
+      <p className="text-white">
+            Sawer Gua{" "}
+            <a href="https://saweria.co/FajarSatBoy">
+              <strong className="underline text-green-300">FajarSatBoy <i class="bi bi-coin"></i></strong>
+            </a>
+          </p>
+      </div>
       <div className="flex justify-center">
         <p className="text-red-500">
           On Developed{" "}
